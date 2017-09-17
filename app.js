35f67a39c5dc4556fde3602f6583933fcce8c16d
Chaitanya <chaitanya@serindlabs.com>
2017-09-16 23:44:18 -0400

    update code

--- app.js
+++ app.js
@@ -1,33 +1,76 @@
-/**
- * Copyright 2017, Google, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
-// [START app]
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+// Google Cloud Services
+const keyFilename="./geo-vune-firebase-adminsdk-2avs2-8a70f70b85"; //replace this with api key file
+const projectId = "geo-vune" //replace with your project id
+const bucketName = `${projectId}.appspot.com`;
+const mime = require('mime-types');
+
+const gcs = require('@google-cloud/storage')({
+     projectId,
+     keyFilename
 });
 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+const bucket = gcs.bucket(bucketName);
+
+// Firebase
+/*var firebase = require('firebase');
+firebase.initializeApp({
+    apiKey: "AIzaSyB894prbl5dG99bPeFBWPA8tVrRjM0SZHg",
+    authDomain: "geo-vune.firebaseapp.com",
+    databaseURL: "https://geo-vune.firebaseio.com",
+    projectId: "geo-vune",
+    storageBucket: "geo-vune.appspot.com",
+    messagingSenderId: "170143223481"
+});*/
+
+const filePath = `./package.json`;
+const uploadTo = `subfolder/package.json`;
+const fileMime = mime.lookup(filePath);
+
+app.get('/', (req, res) => {
+  res.status(200).send('Geo Vune API Demo');
+
+	  bucket.upload(filePath,{
+	    destination:uploadTo,
+	    public:true,
+	    metadata: {contentType: fileMime,cacheControl: "public, max-age=300"}
+	}, function(err, file) {
+	    if(err)
+	    {
+	        console.log(err);
+	        return;
+	    }
+	    console.log(createPublicFileURL(uploadTo));
+	});
+	console.log('Geo-vune')
 });
-// [END app]
+
+function createPublicFileURL(storageName) {
+     return `http://storage.googleapis.com/${bucketName}/${encodeURIComponent(storageName)}`;
+}
+
+app.get('/retrieveVideoURL', (req, res) => {
+ 	console.log(`Retrieving video URL from Firebase`);
+
+});
+
+ app.post('/postVideo', (req, res) => {
+
+});
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8081, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
